0c3e72c36520a4909a753b3a1b2deda7e4eac3ac
Niklas Koll <nikkol567@gmail.com>
2020-10-23 14:45:05 +0200

    Initial Commit

---
+++ Do You Remember?/Do_You_Remember_App.swift
@@ -0,0 +1,17 @@
+//
+//  Do_You_Remember_App.swift
+//  Do You Remember?
+//
+//  Created by Niklas Koll on 23.10.20.
+//
+
+import SwiftUI
+
+@main
+struct Do_You_Remember_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
